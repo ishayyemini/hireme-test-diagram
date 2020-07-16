a442e8d247912946a8e7528ba34beae4627d2179
ishay <ishay.yemini@gmail.com>
2020-07-16 16:15:22 +0300

    Added empty grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+import { Grommet, Box } from 'grommet'
 
 import DiagramBody from './components/DiagramBody'
 
+const Wrapper = styled(Box)`
+  max-width: 100vw;
+  max-height: 100vh;
+`
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+
 const App = () => {
-  return <DiagramBody />
+  return (
+    <Grommet>
+      <GlobalStyle />
+      <Wrapper>
+        <DiagramBody />
+      </Wrapper>
+    </Grommet>
+  )
 }
 
 export default App
--- src/components/DiagramBody.js
+++ src/components/DiagramBody.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Grid, Box } from 'grommet'
+
+const Wrapper = styled(Box)`
+  max-width: 100%;
+  max-height: 100%;
+  overflow: scroll;
+`
 
 const DiagramBody = () => {
-  return <div></div>
+  const [maxX, maxY] = [50, 50]
+  const gridLayout = Array.from(Array(maxX), (x) =>
+    Array.from(Array(maxY), (y) => [x, y])
+  ).flat()
+
+  return (
+    <Wrapper
+      onScroll={(e) => console.log(e.target.scrollTop, e.target.scrollLeft)}
+    >
+      <Grid
+        rows={Array.from(Array(maxY), () => 'xsmall')}
+        columns={Array.from(Array(maxX), () => 'xsmall')}
+        gap={'small'}
+      >
+        {gridLayout.map((item, index) => (
+          <Box
+            background={'lightgray'}
+            style={{ borderRadius: '50%' }}
+            margin={'medium'}
+            key={index}
+          />
+        ))}
+      </Grid>
+    </Wrapper>
+  )
 }
 
 export default DiagramBody
